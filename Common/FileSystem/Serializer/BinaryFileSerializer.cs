c316f76927b4aa876a5e5a08b835264084d43bea
NoCodeNoLife-cloud <1607575130@qq.com>
2025-01-07 00:25:27 +0800

    Feature: Add binary Serialization

---
+++ Common/FileSystem/Serializer/BinaryFileSerializer.cs
@@ -0,0 +1,26 @@
+﻿using MessagePack;
+using MessagePack.Resolvers;
+
+namespace Common.FileSystem.Serializer;
+
+public abstract class BinaryFileSerializer : IFileSerializable
+{
+	public static void SerializeToFile<T>(T obj, string filePath)
+	{
+		if (obj == null) throw new ArgumentNullException(nameof(obj), "obj cannot be null");
+		if (string.IsNullOrEmpty(filePath)) throw new ArgumentException("file path cannot be empty", nameof(filePath));
+
+		var options = MessagePackSerializerOptions.Standard.WithResolver(ContractlessStandardResolver.Instance);
+		var binaryData = MessagePackSerializer.Serialize(obj, options);
+		File.WriteAllBytes(filePath, binaryData);
+	}
+
+	public static T DeserializeFromFile<T>(string filePath)
+	{
+		if (string.IsNullOrEmpty(filePath)) throw new ArgumentException("file path cannot be empty", nameof(filePath));
+
+		var options = MessagePackSerializerOptions.Standard.WithResolver(ContractlessStandardResolver.Instance);
+		var binaryData = File.ReadAllBytes(filePath);
+		return MessagePackSerializer.Deserialize<T>(binaryData, options);
+	}
+}
