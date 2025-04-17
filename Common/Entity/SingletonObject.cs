8c620102c7ac876c05752a2d3d70670e168f8a1f
NoCodeNoLife-cloud <54143278+NoCodeNoLife-cloud@users.noreply.github.com>
2025-04-18 01:53:39 +0800

    upload

---
+++ Common/Entity/SingletonObject.cs
@@ -0,0 +1,20 @@
+﻿namespace Common.Entity;
+
+public static class SingletonObject<T> where T : class
+{
+	private static readonly Lazy<T?> Instance = new Lazy<T?>(() => _initializer?.Invoke());
+
+	private static Func<T>? _initializer;
+
+	public static T? instance => Instance.Value;
+
+	public static void Initialize(Func<T> initializer)
+	{
+		if (_initializer != null)
+		{
+			throw new InvalidOperationException("Singleton instance has already been initialized.");
+		}
+
+		_initializer = initializer ?? throw new ArgumentNullException(nameof(initializer));
+	}
+}
--- Common/Entity/SingletonWrapper.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Common.Entity;
-
-public abstract class SingletonWrapper<T> where T : class, new()
-{
-	private static readonly Lazy<T> Instance = new Lazy<T>(() => new T());
-
-	public static T GetInstance() => Instance.Value;
-}
